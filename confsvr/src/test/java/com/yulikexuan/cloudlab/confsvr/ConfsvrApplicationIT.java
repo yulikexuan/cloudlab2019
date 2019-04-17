332633e919591dd23b15157ee4dd4fcda6f8891f
Yu Li <yu.li@tecsys.com>
2019-04-17 10:39:19 -0400

    Add gitignore file to root

---
+++ confsvr/src/test/java/com/yulikexuan/cloudlab/confsvr/ConfsvrApplicationIT.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.cloudlab.confsvr.ConfsvrApplicationIT.java
+
+
+package com.yulikexuan.cloudlab.confsvr;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class ConfsvrApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}///:~
