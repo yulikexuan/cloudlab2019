0721ea45174d08edca499fc2c3a70b1d66fd8984
yul <yu.li@tecsys.com>
2019-04-19 09:58:30 -0400

    Enable all spring actuator api for limits service

--- limits/src/main/java/com/yulikexuan/cloudlab/limits/api/config/LimitsConfiguration.java
+++ limits/src/main/java/com/yulikexuan/cloudlab/limits/api/config/LimitsConfiguration.java
@@ -6,17 +6,23 @@ package com.yulikexuan.cloudlab.limits.api.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 
 @Getter
 @Setter
+@RefreshScope
 @Component
-@ConfigurationProperties("limits")
+// @ConfigurationProperties("limits")
 public class LimitsConfiguration {
 
+    @Value("${limits.minimum}")
     private int minimum;
+
+    @Value("${limits.maximum}")
     private int maximum;
 
 }///:~
