e48db47f7006826f15feaff7744adfe5eb9a7cf7
yul <yu.li@tecsys.com>
2019-04-22 18:26:23 -0400

    Add currency conversion service project

---
+++ curconv/src/main/java/com/yulikexuan/cloudlab/curconv/CurconvApplication.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.cloudlab.curconv.CurexcApplication.java
+
+
+package com.yulikexuan.cloudlab.curconv;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CurconvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurconvApplication.class, args);
+    }
+
+}///:~
---
+++ curconv/src/test/java/com/yulikexuan/cloudlab/curconv/CurconvApplicationIT.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.cloudlab.curconv.CurconvApplicationIT.java
+
+
+package com.yulikexuan.cloudlab.curconv;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CurconvApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}///:~
