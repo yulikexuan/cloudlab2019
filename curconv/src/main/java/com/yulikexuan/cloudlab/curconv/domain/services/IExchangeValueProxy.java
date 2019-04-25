6374f1c43eb8c9eb9c8ab838aa268a0fa2bf5b6d
Yu Li <yu.li@tecsys.com>
2019-04-25 16:51:26 -0400

    Add ribbon to currency conversion service

--- curconv/src/main/java/com/yulikexuan/cloudlab/curconv/domain/services/IExchangeValueProxy.java
+++ curconv/src/main/java/com/yulikexuan/cloudlab/curconv/domain/services/IExchangeValueProxy.java
@@ -5,12 +5,15 @@ package com.yulikexuan.cloudlab.curconv.domain.services;
 
 
 import com.yulikexuan.cloudlab.curconv.domain.model.CurrencyConversion;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "conexc", url = "localhost:8001")
+// @FeignClient(name = "conexc", url = "localhost:8001")
+@FeignClient(name = "curexc")
+@RibbonClient(name = "curexc")
 public interface IExchangeValueProxy {
 
     @GetMapping("/api/v1/curexc/from/{from}/to/{to}")
