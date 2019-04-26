bb3971689d50695a7e8482732e529645ca8cab9a
yul <yu.li@tecsys.com>
2019-04-26 14:40:40 -0400

    Initialize API Gateway Server driven by Spring-Cloud-Zuul

---
+++ zuulsvr/src/main/java/com/yulikexuan/cloudlab/zuulsvr/ZuulsvrApplication.java
@@ -0,0 +1,13 @@
+package com.yulikexuan.cloudlab.zuulsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulsvrApplication.class, args);
+    }
+
+}
