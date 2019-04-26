583a4706fe90ce205f3f3f7d4e0484a892e7669a
yul <yu.li@tecsys.com>
2019-04-26 14:24:46 -0400

    Make ribbon client balancing with eureka naming server

--- curexc/src/main/java/com/yulikexuan/cloudlab/curexc/CurexcApplication.java
+++ curexc/src/main/java/com/yulikexuan/cloudlab/curexc/CurexcApplication.java
@@ -6,9 +6,13 @@ package com.yulikexuan.cloudlab.curexc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients("com.yulikexuan.cloudlab.curexc.domain.services")
 public class CurexcApplication {
 
     public static void main(String[] args) {
